dc2f5d7c4655ec414d1fb925606322ecac2d5bb9
Raphael Bacher <raphael.bacher@univ-grenoble-alpes.fr>
2018-11-15 08:09:43 +0100

    big commit

--- convert.sh
+++ convert.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-jupyter-nbconvert --to script full-notebooks/intro-to-julia/*.ipynb --template template.tpl
-mv full-notebooks/intro-to-julia/*.jl skeleton-notebooks/intro-julia/
-jupytext --to notebook skeleton-notebooks/intro-julia/*.jl
-rm skeleton-notebooks/intro-julia/*.jl
+# note: need jupytext
+#DIR=intro-to-julia
+DIR=datascience
+
+cp full-notebooks/$DIR/* skeleton-notebooks/$DIR/
+rm skeleton-notebooks/$DIR/*.ipynb
+jupyter-nbconvert --to script full-notebooks/$DIR/*.ipynb --template template.tpl
+mv full-notebooks/$DIR/*.jl skeleton-notebooks/$DIR/
+jupytext --to notebook skeleton-notebooks/$DIR/*.jl
+rm skeleton-notebooks/$DIR/*.jl
